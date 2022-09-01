440db4b73c64bde23db60dd0d9a369ec8cd2ec0f
Harshleen Kaur <harshleenkaur007@gmail.com>
2022-09-02 00:59:36 +0530

    Diff file

---
+++ Diff.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Diff{
+	public static void main(String[] args)
+	{
+		int a=5;
+		int b=5;
+		int diff=a+b;
+		System.out.println("The difference of 5+5 is"+diff);
+	}
+}
